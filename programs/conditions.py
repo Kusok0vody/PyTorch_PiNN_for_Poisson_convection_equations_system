919bc8962ac737f6b5c16bcccc3b59043e6d0f37
Илья <melnikoff280@gmail.com>
2024-06-25 20:06:07 +0700

    support for 2d

--- programs/conditions.py
+++ programs/conditions.py
@@ -1,29 +1,33 @@
-from numpy import vstack, sin, cos, sinh, cosh, power, pi
-from torch import device, cuda, FloatTensor
-devicee = device("cuda:0" if cuda.is_available() else "cpu")
+from numpy import vstack, concatenate, sin, cos, sinh, cosh, power, pi
+from torch import device as d, cuda, FloatTensor
+device = d("cuda:0" if cuda.is_available() else "cpu")
+import numpy as np
 
-
-def form_condiition(x_range, y_range, t_range, arr):
+def form_condiition(ranges:list, arr):
     """
     Forms an array with a condition at given coordinates
     """
 
-    boundary = vstack([x_range, y_range, t_range, arr]).T
+    boundary = vstack(concatenate((arr.reshape(1,-1), ranges))).T
     return boundary
 
 
-def form_boundaries(x_range, y_range, t_range, cond, ones, zeros):
+def form_boundaries(ranges, cond, ones, zeros):
     """
     Generates an array of boundary conditions
 
     Border order – top, bottom, left, right
     """
-
-    u_top = form_condiition(x_range, ones, t_range, cond[0])
-    u_bottom = form_condiition(x_range, zeros, t_range, cond[1])
-    u_left = form_condiition(zeros, y_range, t_range, cond[2])
-    u_right = form_condiition(ones, y_range, t_range, cond[3])
-
+    if len(ranges)==3:
+        u_top = form_condiition([ranges[0], ones, ranges[2]], cond[0])
+        u_bottom = form_condiition([ranges[0], zeros, ranges[2]], cond[1])
+        u_left = form_condiition([zeros, ranges[1], ranges[2]], cond[2])
+        u_right = form_condiition([ones, ranges[1], ranges[2]], cond[3])
+    if len(ranges)==2:
+        u_top = form_condiition([ranges[0], ones], cond[0])
+        u_bottom = form_condiition([ranges[0], zeros], cond[1])
+        u_left = form_condiition([zeros, ranges[1]], cond[2])
+        u_right = form_condiition([ones, ranges[1]], cond[3])   
     return vstack([u_top, u_bottom, u_left, u_right])
 
 
@@ -32,17 +36,12 @@ def form_condition_arrays(BC_arr):
     Forms separate arrays with coordinates and condition
     """
 
-    x = BC_arr[:, 0]
-    y = BC_arr[:, 1]
-    t = BC_arr[:, 2]
-    f = BC_arr[:, 3]
+    f = FloatTensor(BC_arr[:, 0]).to(device)
+    X = []
+    for i in range(1,len(BC_arr[0])):
+        X.append(FloatTensor(BC_arr[:,i]).to(device))
     
-    x = FloatTensor(x).to(devicee)
-    y = FloatTensor(y).to(devicee)
-    t = FloatTensor(t).to(devicee)
-    f = FloatTensor(f).to(devicee)
-
-    return f, x, y, t
+    return f, X
 
 
 def Poisson_analytical(mu, Q0, w, x, y, chi, H, L, K):
