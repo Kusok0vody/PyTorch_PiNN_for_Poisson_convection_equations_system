80bafbe15d956f7b769221347e67d9f3df26af6c
Илья <melnikoff280@gmail.com>
2024-11-15 18:06:09 +0700

    removed classes and functions
    no more load/save functions;
    classes are now in objects.py;
    derivative in misc

--- programs/NN.py
+++ programs/NN.py
@@ -1,39 +1,10 @@
-import numpy as np 
 import torch
 import torch.nn as nn
+import programs.objects as obj
 
-from torch.autograd import grad
 from collections import OrderedDict
 
 torch.manual_seed(1234)
-
-class Sin(nn.Module):
-    """
-    sin activation function for Neural Network
-    """
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, input):
-        return torch.sin(input)
-    
-
-class Wave(nn.Module):
-    """
-    sin+cos activation function for Neural Network
-    """
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, input):
-        return torch.cos(input) + torch.sin(input)
-
-
-class OutputHook(list):
-    """ Hook to capture module outputs.
-    """
-    def __call__(self, module, input, output):
-        self.append(output)
 
 
 class Net(nn.Module):
@@ -46,7 +17,8 @@ class Net(nn.Module):
         neurons_arr:list,
         output_size:int,
         depth:int,
-        act,):
+        act
+        ):
 
         super(Net, self).__init__()
         
@@ -61,7 +33,7 @@ class Net(nn.Module):
 
         layerDict = OrderedDict(layers)
         self.layers = torch.nn.Sequential(layerDict)
-        self.sin = Sin()
+        self.sin = act
 
     def forward(self, inputs: list, transform_func=None):
         """
@@ -71,58 +43,14 @@ class Net(nn.Module):
         inputs_united = torch.cat([input_tensor.reshape(-1, 1) for input_tensor in inputs], axis=1)
         outputs = self.layers(inputs_united)
         if transform_func!=None:
-            transform_func(outputs)
+            transform_func(outputs, inputs)
         return outputs
-        
     
+    def set_optimizer(self, name):
+        self.optimizer = obj.Optimizer(name, self.parameters)
+
     @staticmethod
     def init_weights(m):
         if isinstance(m, nn.Linear):
             torch.nn.init.xavier_uniform_(m.weight)
             m.bias.data.fill_(0.01)
-
-
-    def set_optimizer(self, optimizer_type:str):
-        """
-        Sets the optimizer for Neural Network
-        
-        Types: Adam, LBFGS
-        """
-        if optimizer_type=='Adam':
-            return torch.optim.Adam(self.parameters(), weight_decay=1e-5)
-        if optimizer_type=='NAdam':
-            return torch.optim.NAdam(self.parameters(), weight_decay=1e-5)
-        if optimizer_type=='LBFGS':
-            return torch.optim.LBFGS(self.parameters(),
-                                     # lr=0.1, 
-                                     max_iter=100000, 
-                                     max_eval=100000, 
-                                     history_size=200,
-                                     tolerance_grad=1e-12, 
-                                     tolerance_change=0.5 * np.finfo(float).eps,
-                                     line_search_fn="strong_wolfe")
-
-
-    @staticmethod
-    def derivative(dx, x, order=1):
-        """
-        Calculates the derivative of a given array
-        """
-        for _ in range(order):
-            dx = grad(outputs=dx, inputs=x, grad_outputs = torch.ones_like(dx), create_graph=True, retain_graph=True)[0]
-
-        return dx
-    
-
-    def load_model(self, path:str):
-        """
-        Loads NN from file
-        """
-        self.load_state_dict(torch.load(path))
-
-
-    def save_model(self, path:str):
-        """
-        Saves NN to file
-        """
-        torch.save(self.state_dict(), path)
